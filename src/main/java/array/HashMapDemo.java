ad37a3cddba9725828e349fcf568c4724e4b823f
Mani Arsham <moneyfx@gmail.com>
2017-06-04 20:58:20 -0400

    hashmap

---
+++ src/main/java/array/HashMapDemo.java
@@ -0,0 +1,26 @@
+package array;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapDemo {
+    public static void main(String[] args) {
+        Map<String, Integer> hm = new HashMap<String, Integer>();
+
+        hm.put("key1", 1);
+        hm.put("key2", 2);
+
+        System.out.println(hm);
+
+        if (hm.containsKey("key1")) {
+            System.out.println("hm has key1 with value = " + hm.get("key1"));
+        } else {
+            System.out.println("hm does not contain key1");
+        }
+
+        if (hm.get("key2") != null) {
+            System.out.println("value of key2 is not null and is equal to " + hm.get("key2"));
+        }
+    }
+}
